8e7c5916e585ba49d32518776ddbc12fa944c88b
Vanessa <jvtrivinog@unal.edu.co>
2020-06-15 02:41:44 -0500

    :construction: Get de intereses por usuario

--- Controllers/InteresController.cs
+++ Controllers/InteresController.cs
@@ -41,6 +41,13 @@ namespace backend.Controllers
             return interes;
         }
 
+        //GET: api/Interes/usuario/3
+        [HttpGet("usuario/{id}")]
+        public async Task<ActionResult<IEnumerable<Interes>>> GetReservaByUserId(int id)
+        {
+            return await _context.Interes.Include(x => x.Publicacion).Where(x => x.Usuario.Id == id).ToListAsync();
+        }
+
         // PUT: api/Interes/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
